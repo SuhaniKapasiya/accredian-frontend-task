50d66ed397d5fcd2e84d3323c3a60bacdd9c7748
SuhaniKapasiya <107610361+SuhaniKapasiya@users.noreply.github.com>
2025-02-14 23:18:45 +0530

    added backend hosted URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000"});
+  baseURL: "https://accredian-backend-task-nnpe.onrender.com",
+});
 
 // authenticaton API
 export const referapi = (data) => API.post("/api/refer", data);
