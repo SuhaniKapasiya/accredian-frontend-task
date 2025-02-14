8c8e6cc28e59a8544c703595bcf4fd62c6d89bf7
SuhaniKapasiya <107610361+SuhaniKapasiya@users.noreply.github.com>
2025-02-15 00:13:42 +0530

    update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://accredian-backend-task-nnpe.onrender.com",
+  baseURL: "https://accredian-backend-task-nnpe.onrender.com/",
 });
 
 // authenticaton API
-export const referapi = (data) => API.post("/api/refer", data);
+export const referapi = (data) => API.post("api/refer", data);
